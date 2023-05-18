946418e2bdf41decc9abccea66ec5a3173379815
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-05-18 19:48:14 -0400

    Added method [kimcheaw] to IntroLab and relevant test in IntroLabTest.

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 100)
+    public void testKimcheaw() {
+        String expected = "Hi! My name is Cheawon Kim! " +
+                "I like cats more than dogs! For real.";
+        String actual = IntroLab.kimcheaw();
+        assertEquals(expected, actual);
+    }
 }
