b8a103c7732c95560cc5358ca8db050f08ca4962
NotSakura <sakura.noskor@mail.utoronto.ca>
2023-05-22 14:23:12 -0400

    Added noskorsh() and testNoskorshStudent() in each respective file

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -33,10 +33,7 @@ public class IntroLab {
         return message_to_print + message;
     }
 
-    /* TODO: In the space below, write your our method.
-     *       The method should be named with your utorID and should call
-     *       printLabMessage with some parameters.
-     *
+    /*
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
